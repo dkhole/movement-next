43f2ee2a0e3767e8a1a137d6fe32c57e57bbb8ce
dkhole <dkhlee419@gmail.com>
2022-06-20 18:34:01 +1000

    update SEO title and description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,15 +7,17 @@ import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  gsap.registerPlugin(ScrollTrigger);
-  return (
-    <>
-      <Head>
-        <title>Movement-Mechanics</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+	gsap.registerPlugin(ScrollTrigger);
+	return (
+		<>
+			<Head>
+				<title>Movement Mechanics</title>
+				<meta name="description" content="Movement Mechanics is a physiotherapy and chiropractic clinic based in Newington." />
+				<link rel="canonical" href="http://www.movement-mechanics.com.au/" />
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
 
 export default MyApp;
